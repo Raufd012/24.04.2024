844653c0a4fe1397439d44893a5e1cea4478e9b8
Rauf <161576550+Raufd012@users.noreply.github.com>
2024-04-24 00:55:05 +0400

    initial commit

---
+++ HomeTask23/HomeTask23/Exam.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask23
+{
+    internal class Exam
+    {
+		private string _subject;
+
+		public string Subject
+		{
+			get { return _subject;
+
+; }
+			set { _subject = value; }
+		}
+		private int _examDuraction;
+
+		public int ExamDuraction
+		{
+			get { return _examDuraction; }
+			set { _examDuraction = value; }
+		}
+
+		public DateTime StartDate {  get; set; }
+		public DateTime EndDate { get; set; }
+		public Exam(string subject, int examduraxtion, DateTime start)
+		{
+			_subject = subject;
+			_examDuraction = examduraxtion;
+			StartDate = start;
+			EndDate = start.AddMinutes(examduraxtion);
+		}
+	}
+}
---
+++ HomeTask23/HomeTask23/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace HomeTask23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Exam> tasks = new List<Exam>();
+            Exam exam1 = new Exam("Yazili Imtahan", 3000, DateTime.Now);
+            Exam exam2 = new Exam("Kollokvium Imtahan", 30, DateTime.Now);
+            tasks.Add(exam1);
+            tasks.Add(exam2);
+            tasks.FindAll(s => s.StartDate <= DateTime.Now.AddDays(7)).ForEach(s => Console.WriteLine(s.Subject + " " + s.ExamDuraction));
+            tasks.FindAll(s => s.ExamDuraction > 120).ForEach(s => Console.WriteLine(s.Subject + " " + s.ExamDuraction));
+            tasks.FindAll(s => DateTime.Now >= s.StartDate && DateTime.Now < s.EndDate).ForEach(s => Console.WriteLine(s.Subject + " " + s.ExamDuraction));
+            List<Person> people = new List<Person>()
+            {
+            new Person { Name = "Kamil", Surname = "Qafarov", Age = 25 },
+            new Person { Name = "Rauf", Surname = "Melikov", Age = 18 },
+            new Person { Name = "Sabina", Surname = "Katanova", Age = 30 }
+    };
+
+            List<Person> kamilPeople = people.FindAll(p => p.Name == "Kamil");
+            Console.WriteLine("People named Kamil:");
+            foreach (Person person in kamilPeople)
+            {
+                Console.WriteLine($"\t- {person.Name} {person.Surname} ({person.Age})");
+            }
+
+            List<Person> ovPeople = people.FindAll(p => p.Surname.EndsWith("ov") || p.Surname.EndsWith("ova"));
+            Console.WriteLine("\nPeople with surnames ending in 'ov' or 'ova':");
+            foreach (Person person in ovPeople)
+            {
+                Console.WriteLine($"\t- {person.Name} {person.Surname} ({person.Age})");
+            }
+            List<Person> adultPeople = people.FindAll(p => p.Age >= 20);
+            Console.WriteLine("\nPeople with age 20 or above:");
+            foreach (Person person in adultPeople)
+            {
+                Console.WriteLine($"\t- {person.Name} {person.Surname} ({person.Age})");
+            }
+        }
+
+    }
+    
+}
